e2e810a8d67bcc62f89ca809fa8b1f141ab05b83
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-05-26 21:49:15 +0200

    last commit

--- Moduleweek4/Week4/routes/web.php
+++ Moduleweek4/Week4/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,4 +10,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', [TestController::class, 'index']);
+Route::get('/songs', [SongController::class, 'index'])->name('songs');
+// Route::get('/song/{id}', [SongController::class, 'show'])->name('show');
+// Route::get('/song/create', [SongController::class, 'create'])->name('create');
+// Route::get('/song/{id}/edit', [SongController::class, 'show'])->name('show');
+
+
